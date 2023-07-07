d86da635e9362c6300efffce6b2b6c21265ceeb6
Super-WeiDong <weidongwang@brandeis.edu>
2023-07-07 19:11:58 -0400

    modify listener

--- src/main/java/com/example/kafka/config/KafkaListener.java
+++ src/main/java/com/example/kafka/config/KafkaListener.java
@@ -5,7 +5,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class KafkaListener {
 
-    @org.springframework.kafka.annotation.KafkaListener(topics ="dream",groupId = "groupId")
+    //kafkaListenerContainerFactory refers the type of bean we create in ConsumerConfig
+    @org.springframework.kafka.annotation.KafkaListener(topics ="dream",groupId = "groupId", containerFactory = "kafkaListenerContainerFactory")
     public void listener(String data){
         System.out.println("Listener received data " + data + " !!!");
     }
